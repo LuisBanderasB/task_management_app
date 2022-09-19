0358e0af218c310795b7ba41388c37beb1a33e1d
LuisBanderasB <luis.banderas@enroutesystems.com>
2022-09-19 16:50:08 -0500

    validations and seed boards

--- app/models/board.rb
+++ app/models/board.rb
@@ -1,3 +1,4 @@
 class Board < ApplicationRecord
-  
+  validates :name, presence: true, length: {minimum: 6, maximum: 100}
+  validates :visibility, presence: true, length: {minimum: 6, maximum: 8}
 end
