d732414f5ff9d1529c4bacbd1d4d83d1307d2ad0
LuisBanderasB <luis.banderas@enroutesystems.com>
2022-09-23 12:44:01 -0500

    add board

--- app/controllers/boards_controller.rb
+++ app/controllers/boards_controller.rb
@@ -6,9 +6,24 @@ class BoardsController < ApplicationController
   end
 
   def show
+    @lists = @board.lists
+  end
+
+  def create
+    @board = Board.new(board_params)
+    if @board.save
+      flash[:notice] = "Board was created successfully." 
+      redirect_to @board
+    else
+      render 'index'
+    end
   end
 
   private 
+
+  def board_params 
+    params.require(:board).permit(:name, :visibility)
+  end
 
   def set_board
     @board = Board.find(params[:id])
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,4 @@
 Rails.application.routes.draw do
   root 'welcome#index'
-  resources :boards, only: [:index, :show]
+  resources :boards, only: [:index, :show, :create]
 end
