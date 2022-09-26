e1d9de80634d696396188fd3d1e875a425905c92
LuisBanderasB <luis.banderas@enroutesystems.com>
2022-09-26 11:44:37 -0500

    add task

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,5 +1,16 @@
-class TaskController < ApplicationController
+class TasksController < ApplicationController
   def create
-    byebug
+      @task = Task.new(task_params)
+      if @task.save
+        flash[:notice] = "Board was created successfully." 
+      else
+        render 'index'
+      end
+  end
+
+  private 
+
+  def task_params 
+    params.require(:task).permit(:title, :list_id)
   end
 end
--- app/javascript/packs/custom.js
+++ app/javascript/packs/custom.js
@@ -1,6 +1,6 @@
 $(function() {
   $('a#show_whatever').click(function(event){
     event.preventDefault();
-    $('div#whatever').toggle();
+    $('div.hidden').toggle();
   });
 });   
