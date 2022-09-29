5325d18650faee6e29c86961d8496c7052ec2e8d
LuisBanderasB <luis.banderas@enroutesystems.com>
2022-09-28 21:34:25 -0500

    fix problems displaying add task form

--- app/javascript/packs/custom.js
+++ app/javascript/packs/custom.js
@@ -1,6 +1,17 @@
-$(function() {
-  $('#show').click(function(event){
+// $(function() {
+//   $('#show').click(function(event){
+//     event.preventDefault();
+//     $('div.hidden').toggle();
+//   });
+// });   
+
+document.querySelectorAll('a.add').forEach(occurence => {
+  occurence.addEventListener('click', (e) => {
+    let parentId = e.target.parentElement.id;
+    let id = `form_${parentId.substring(parentId.indexOf('_') + 1)}`;
+
     event.preventDefault();
-    $('div.hidden').toggle();
+    $("div"+"#"+parentId+".hidden").toggle();
+    $("div"+"#"+id+".hidden").toggle();
   });
-});   
+});
