353575aa9ad8c02749a9b4c80d61842e570bb9e4
LuisBanderasB <luis.banderas@enroutesystems.com>
2022-09-28 22:04:11 -0500

    fix problems

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,13 +1,14 @@
 class TasksController < ApplicationController
   before_action :set_task, only: [:update]
+  before_action :set_board, only: [:create]
 
   def create
-      @task = Task.new(task_params)
-      if @task.save
-        redirect_to board_path(task_params[:list_id])
-      else
-        flash[:danger] = "Something went wrong." 
-      end
+    @task = Task.new(task_params)
+    if @task.save
+      redirect_to @board
+    else
+      flash[:danger] = "Something went wrong." 
+    end
   end
 
   def update
@@ -20,8 +21,11 @@ class TasksController < ApplicationController
     @task = Task.find(params[:id])
   end
 
+  def set_board
+    @board = List.find(task_params[:list_id]).board
+  end
+
   def task_params 
-    byebug
     params.require(:task).permit(:title, :list_id)
   end
 end
